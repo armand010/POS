8fc0719e5f3e7c6a4c76b1241b0db88ead74699c
Armand Maulana Andika Putra <andikaputrasamawa@gmail.com>
2024-03-03 10:32:13 +0700

    menambahkan output tulisan pada views

---
+++ resources/views/category/beauty-health.blade.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<head>
+</head>
+<body>
+    <h1>Halaman Beauty-Health</h1>
+</body>
+</html>
---
+++ resources/views/category/home-care.blade.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<head>
+</head>
+<body>
+    <h1>Halaman Home-Care</h1>
+</body>
+</html>
